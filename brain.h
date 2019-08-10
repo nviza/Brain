67f346c70f09b0cbb79ef8f9bb36f67000047e72
Nixon VIZA <nviza@e2r13p8.42.fr>
2019-08-10 19:59:09 +0200

    laviecestdure

---
+++ brain.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   brain.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nviza <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/10 18:00:00 by nviza             #+#    #+#             */
+/*   Updated: 2019/08/10 18:14:34 by nviza            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define brain.h
+
+#include <unistd.h>
+
+int	ft_possible(int number, int colum, int ligne)
+int	st_str(char *str)
+int	ft_verif_line(int arg)
+int	ft_validate_line(char *str)
+
+void	buildtable(void)
+void	print_table(void)
